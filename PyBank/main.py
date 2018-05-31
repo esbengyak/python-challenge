3b3b2464ee68819488f983f893ad8df265047b84
esbengyak <esbengyak@gmail.com>
2018-05-31 15:01:25 -0700

    Script Added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,11 @@
-# PyBank Homework Solution."""
-
-# Dependencies
+# PyBank Homework Solution
 import csv
 
-# Files to load and output (Remember to change these)
-file_to_load = "raw_data/budget_data_1.csv"
-file_to_output = "analysis/budget_analysis_1.txt"
+data_one = csv.read("budget_data_1.csv")
+data_two = csv.read("budget_data_2.csv")
 
-# Track various revenue parameters
+file_to_output = "main.py"
+
 total_months = 0
 prev_revenue = 0
 month_of_change = []
@@ -16,36 +14,29 @@ greatest_increase = ["", 0]
 greatest_decrease = ["", 9999999999999999999]
 total_revenue = 0
 
-# Read the csv and convert it into a list of dictionaries
-with open(file_to_load) as revenue_data:
+with open(data_one) as revenue_data:
     reader = csv.DictReader(revenue_data)
 
     for row in reader:
 
-        # Track the total
         total_months = total_months + 1
         total_revenue = total_revenue + int(row["Revenue"])
 
-        # Track the revenue change
         revenue_change = int(row["Revenue"]) - prev_revenue
         prev_revenue = int(row["Revenue"])
         revenue_change_list = revenue_change_list + [revenue_change]
         month_of_change = month_of_change + [row["Date"]]
 
-        # Calculate the greatest increase
         if (revenue_change > greatest_increase[1]):
             greatest_increase[0] = row["Date"]
             greatest_increase[1] = revenue_change
-
-        # Calculate the greatest decrease
+            
         if (revenue_change < greatest_decrease[1]):
             greatest_decrease[0] = row["Date"]
             greatest_decrease[1] = revenue_change
 
-# Calculate the Average Revenue Change
 revenue_avg = sum(revenue_change_list) / len(revenue_change_list)
 
-# Generate Output Summary
 output = (
     f"\nFinancial Analysis\n"
     f"----------------------------\n"
@@ -55,7 +46,6 @@ output = (
     f"Greatest Increase in Revenue: {greatest_increase[0]} (${greatest_increase[1]})\n"
     f"Greatest Decrease in Revenue: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
 
-# Print the output (to terminal)
 print(output)
 
 # Export the results to text file
